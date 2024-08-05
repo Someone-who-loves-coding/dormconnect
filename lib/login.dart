88ffb6035d263affaaf176a7feb1ef49c6e12697
Someone-who-loves-coding <ps2284@srmist.edu.in>
2024-08-05 15:34:09 +0530

    Basic updates

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pinput/pinput.dart';
 import 'otp.dart';
-import 'package:country_code_picker/country_code_picker.dart';
+import 'package:image/image.dart' as img;
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -11,84 +11,63 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-   late String _mobile, _ccode;
+  late String _mobile;
+  late String _ccode;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Center(child: Text("DormConnect")),
-          backgroundColor: Colors.purple.shade100,
-      ),
-      body: Stack(
-        children: [
-          Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/Background.png'),
-                fit: BoxFit.cover,
-              )
-          ),
-          child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            backgroundColor: Colors.transparent,
-            body: Column(
-              children: [
-                const SizedBox(
-                  height: 100,
-                ),
-                const Text("Mobile Number"),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    CountryCodePicker(
-                      backgroundColor: Colors.white,
-                      textStyle: TextStyle(
-                        color: Colors.black
-                      ),
-                      flagWidth: 12,
-                      onChanged: (val) {
-                        _ccode = val.toString();
-                      },
-                      favorite: ['+91','IN'],
-                    ),
-                    Expanded(
-                      child: Pinput(
-                        length: 10,
-                        autofocus: true,
-                        onChanged: (val) {
-                          setState(() {
-                            _mobile = val.toString();
-                          });
-                        },
-                        onCompleted: (val) {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const OTP()));
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(shape: const CircleBorder(side: BorderSide())),
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const OTP()));
-                        },
-                        child: const Icon(Icons.navigate_next_rounded)
-                    ),
-                  ),
-                ),
-                const Padding(
-                    padding: EdgeInsets.only(bottom: 20,right: 40)
-                )
-              ],
-            ),
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/Background.png'),
+            fit: BoxFit.cover,
           ),
         ),
-      ]
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.transparent,
+          body: Column(
+            children: [
+              const SizedBox(height: 258),
+              const Text(
+                "Enter Phone Number:",
+                style: TextStyle(
+                  color: Color(0xFFCFC6E1),
+                  fontSize: 28,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Pinput(
+                length: 10,
+                autofocus: true,
+                defaultPinTheme: PinTheme(
+                  padding: const EdgeInsets.all(1),
+                  height: 37,
+                  width: 37,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFCFC6E1),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onChanged: (val) {
+                  setState(() {
+                    _mobile = val;
+                  });
+                },
+                onCompleted: (val) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => OTP(_mobile)));
+                },
+              ),
+              const Spacer(),
+              Image.asset(
+                "assets/images/moon.png",
+                fit: BoxFit.cover, // Adjust the fit as needed
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/otp.dart
+++ lib/otp.dart
@@ -1,9 +1,13 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:dormconnect/login.dart';
 import 'package:pinput/pinput.dart';
 
 class OTP extends StatefulWidget {
-  const OTP({super.key});
+  final String _mobile;
+
+
+  OTP(this._mobile, {super.key});
 
   @override
   State<OTP> createState() => _OTPState();
@@ -14,21 +18,72 @@ class _OTPState extends State<OTP> {
   Widget build(BuildContext context) {
     late String _otp;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Pinput(
-            length: 4,
-            onChanged: (val) {
-              setState(() {
-                _otp = val;
-              });
-            },
-            onCompleted: (val) {
-
-            },
-          )
-        ],
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/Background.png'),
+              fit: BoxFit.cover,
+            )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.only(left: 40)),
+                Text(
+                  "OTP sent to \n ${widget._mobile}",
+                  style: const TextStyle(
+                    color: Color(0xFFCFC6E1),
+                    fontSize: 28,
+                    fontFamily: "Times New Roman"
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.only(left: 40)),
+                Text(
+                  "Enter OTP to login",
+                  style: TextStyle(
+                    color: Color(0xFFCFC6E1),
+                    fontSize: 28,
+                    fontFamily: "Times New Roman"
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Pinput(
+              length: 6,
+              onChanged: (val) {
+                setState(() {
+                  _otp = val;
+                });
+              },
+              onCompleted: (val) {
+                  if(val == _otp){
+                    print(true);
+                  }
+                  else print(false);
+              },
+            ),
+            const Spacer(),
+            Image.asset(
+              "assets/images/moon.png",
+              fit: BoxFit.cover, // Adjust the fit as needed
+            )
+          ],
+        ),
       ),
     );
   }
